f730cc6b8a79188890d709d9330b6a0a7a481a0f
walking-jw <aurora.jw2@gmail.com>
2021-07-24 15:07:27 +0900

    Create Packages
    Create Packages
    * Activity
    * Common
    
    - Park Jaewon

--- app/src/main/java/com/teambook/mybookdiary/Activity/MainActivity.java
+++ app/src/main/java/com/teambook/mybookdiary/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.teambook.mybookdiary;
+package com.teambook.mybookdiary.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.teambook.mybookdiary.R;
 
 public class MainActivity extends AppCompatActivity {
 
